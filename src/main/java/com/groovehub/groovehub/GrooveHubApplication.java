2732d813fdc0f980e01d5790ac9bb760f71272bf
AD17YAKR <adityasingh.110601@gmail.com>
2024-12-26 00:42:20 +0530

    Initial commit

---
+++ src/main/java/com/groovehub/groovehub/GrooveHubApplication.java
@@ -0,0 +1,13 @@
+package com.groovehub.groovehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrooveHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrooveHubApplication.class, args);
+    }
+
+}
