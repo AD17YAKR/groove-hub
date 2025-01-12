edf491f7695fd0a9f158b39f18e46f6854e3d11b
AD17YAKR <adityasingh.110601@gmail.com>
2025-01-12 22:56:09 +0530

    feature: added docker, initialized https and other security configuration settings

--- src/main/java/com/groovehub/groovehub/GrooveHubApplication.java
+++ src/main/java/com/groovehub/groovehub/GrooveHubApplication.java
@@ -1,4 +1,4 @@
-package com.groovehub.common;
+package com.groovehub.groovehub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
